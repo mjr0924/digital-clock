eba1fe23d19c6db58632143fe7fcff7d78408006
mohammad javad rezaei <45309579+mjr0924@users.noreply.github.com>
2021-04-02 18:01:23 +0430

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 let time = document.querySelector('span');
-setInterval(function(){
+
+setInterval(function()
+{
     let date = new Date();
+    
     let h = date.getHours();
     let m = date.getMinutes();
     let s = date.getSeconds();
+    
     h = (h < 10) ? ('0'+h) : h;
     m = (m < 10) ? ('0'+m) : m;
     s = (s < 10) ? ('0'+s) : s;
+    
     time.innerText=`${h}:${m}:${s} ${(h<12) ? 'AM' : 'PM'}`;
 },1000);
